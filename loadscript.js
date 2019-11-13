87fe19169df34dab208648a6e51b1fe7eccd5d50
s.samuel.bernal <s.samuel.bernal@cab-107cte-17.redclay.k12.de.us>
2019-11-13 11:45:25 -0500

    can now input seed

--- loadscript.js
+++ loadscript.js
@@ -1,16 +1,47 @@
 function makedoc() {
+        var fromhtml = document.getElementById("seedInput").value;
+        if (fromhtml == 100000000) {
         var seed = Math.random()*900000000 + 100000000;
+        } else {
+            seed = fromhtml;
+        }
         seed = seed - seed%1;
         var first = seed%10;
         var second = (seed %100 - seed%10)/10;
         var third = (seed %1000 - seed%100)/100;
+        var fourth = (seed %10000 - seed%1000)/1000;
+        var fifth = (seed %100000 - seed%10000)/10000;
+        var sixth = (seed %1000000 - seed%100000)/100000;
+        var seventh = (seed %10000000 - seed%1000000)/1000000;
+        var eighth = (seed %100000000 - seed%10000000)/10000000;
+        var ninth = (seed %1000000000 - seed%100000000)/100000000;
 
         var fillertext = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus in hac habitasse platea dictumst vestibulum rhoncus. Facilisi nullam vehicula ipsum a arcu cursus. In mollis nunc sed id semper risus in. Parturient montes nascetur ridiculus mus mauris vitae ultricies leo. Vel risus commodo viverra maecenas accumsan lacus. Ac felis donec et odio pellentesque diam volutpat commodo sed. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam. Tellus at urna condimentum mattis pellentesque id. Enim neque volutpat ac tincidunt vitae semper quis. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Tristique nulla aliquet enim tortor at auctor urna nunc id. Sit amet nisl purus in mollis nunc. Consequat semper viverra nam libero. Pretium viverra suspendisse potenti nullam. Congue mauris rhoncus aenean vel elit scelerisque mauris. Ac turpis egestas integer eget aliquet. Pretium vulputate sapien nec sagittis aliquam. Eros donec ac odio tempor orci dapibus. Dui nunc mattis enim ut tellus. Quam vulputate dignissim suspendisse in est ante in. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Eu augue ut lectus arcu. Orci eu lobortis elementum nibh tellus molestie nunc non. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. Egestas dui id ornare arcu odio ut sem nulla pharetra. Mauris a diam maecenas sed enim ut sem viverra aliquet. Feugiat in fermentum posuere urna nec tincidunt praesent semper feugiat. Tortor id aliquet lectus proin nibh nisl condimentum id. Vel pretium lectus quam id. Nisi vitae suscipit tellus mauris a diam maecenas sed. Volutpat blandit aliquam etiam erat velit scelerisque in. A diam maecenas sed enim ut sem viverra aliquet. Non tellus orci ac auctor augue mauris augue neque. Ullamcorper sit amet risus nullam eget felis. Ornare aenean euismod elementum nisi quis. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Ornare quam viverra orci sagittis eu. Eget sit amet tellus cras adipiscing enim. Etiam sit amet nisl purus in mollis nunc sed. Elementum pulvinar etiam non quam lacus suspendisse. Etiam erat velit scelerisque in dictum non consectetur a. Lectus magna fringilla urna porttitor rhoncus dolor purus non enim. Ultricies mi eget mauris pharetra et. Lectus magna fringilla urna porttitor rhoncus dolor purus non enim. Ultrices vitae auctor eu augue ut lectus arcu bibendum. Sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum. Sagittis vitae et leo duis ut diam. Pellentesque pulvinar pellentesque habitant morbi. Euismod elementum nisi quis eleifend quam adipiscing. Fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Aenean sed adipiscing diam donec adipiscing tristique risus. Eget aliquet nibh praesent tristique magna. Elementum tempus egestas sed sed risus pretium quam vulputate. Nunc sed augue lacus viverra vitae congue. Viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Aliquam ultrices sagittis orci a. Blandit libero volutpat sed cras ornare. Neque viverra justo nec ultrices dui sapien eget mi proin. Quam elementum pulvinar etiam non. Sagittis nisl rhoncus mattis rhoncus. Leo urna molestie at elementum eu facilisis. Lorem sed risus ultricies tristique nulla. Eu scelerisque felis imperdiet proin fermentum leo vel orci. Sodales ut etiam sit amet nisl purus in mollis. Laoreet sit amet cursus sit amet dictum sit amet justo. Malesuada proin libero nunc consequat interdum. Est lorem ipsum dolor sit. Iaculis nunc sed augue lacus viverra vitae. Blandit libero volutpat sed cras ornare. Lacus sed viverra tellus in hac habitasse platea. At in tellus integer feugiat scelerisque. Pulvinar mattis nunc sed blandit libero volutpat sed.";
+
+
+        var margin;
+        if (ninth <= 1) {
+            document.body.style.paddingLeft = "10%";
+            document.body.style.paddingRight = "10%";
+            margin = 20;
+        } else if (2 <= ninth && ninth <= 3) {
+            document.body.style.paddingLeft = "7.5%";
+            document.body.style.paddingRight = "7.5%";
+            margin = 15;
+        } else if (4 <= ninth && ninth <= 5) {
+            document.body.style.paddingLeft = "5%";
+            document.body.style.paddingRight = "5%";
+            margin = 10;
+        } else if (6 <= ninth && ninth <= 7) {
+            document.body.style.paddingLeft = "2.5%";
+            document.body.style.paddingRight = "2.5%";
+            margin = 5;
+        }
 
         if (first > 0) { //Nav Tree
         var navcont = document.createElement("div");
             navcont.style.height = "90px";
-            navcont.style.width = "100vw";
+            navcont.style.width = (100 - margin) + "vw";
             navcont.style.display = "flex";
             navcont.style.justifyContent = "space-around";
             navcont.style.alignItems = "center";
@@ -68,7 +99,7 @@ function makedoc() {
                 } else {
                     var splashcont = document.createElement("div");
                     splashcont.style.height = "100vh";
-                    splashcont.style.width = "100vw";
+                    splashcont.style.width = (100 - margin) + "vw";
 
                     var iwid = parseInt((200 + Math.random()*800), 10);
                     var ihigh = parseInt((200 + Math.random()*800), 10);
@@ -93,7 +124,7 @@ function makedoc() {
             } else if (2 <= first && first <= 4) {
                 var splashcont = document.createElement("div");
                 splashcont.style.height = "40vh";
-                splashcont.style.width = "100vw";
+                splashcont.style.width = (100 - margin) + "vw";
                 splashcont.setAttribute("id", "headdiv");
 
                 var img = document.createElement("img");
@@ -117,7 +148,7 @@ function makedoc() {
             } else if (5 <= first && first <= 6) {
                 var splashcont = document.createElement("div");
                 splashcont.style.height = "40vh";
-                splashcont.style.width = "100vw";
+                splashcont.style.width = (100 - margin) + "vw";
                 splashcont.setAttribute("id", "headdiv");
 
                 var img = document.createElement("img");
@@ -174,29 +205,49 @@ function makedoc() {
                 var g;
                 for (g = 0; g < 3; g++) {
                     var coldiv = document.createElement("div");
-                    coldiv.style.width = "30vw";
-                    coldiv.style.height = "100px";
+                    coldiv.style.width = "25vw";
+                    coldiv.style.height = "auto";
                     coldiv.style.backgroundColor = "blue";
                     coldiv.style.display = "flex";
-                    coldiv.style.flexdirection = "column";
+                    coldiv.style.flexDirection = "column";
                     coldiv.setAttribute("id", "coldiv" + g);
 
                     document.getElementById("contentcont").appendChild(coldiv);
+                } //three columns
+
+                if (fourth >= 5) {
+                    var p;
+                    for (p = 0; p < 3*4; p++) {
+                        if (fifth >= 5){
+                            var loremcounter = 300*p;
+                            var paragraph = document.createElement("p");
+                            paragraph.style.width = "23vw";
+                            paragraph.style.margin = "0px";
+                            paragraph.style.padding = "10px";
+                            paragraph.innerHTML = fillertext.substring(loremcounter, 300*(p+1));
+                            document.getElementById("coldiv" + p%3).appendChild(paragraph);
+                        } else {
+
+                        }
+                    }
+                } else {
+
                 }
+
             } else if (5 <= third && third <= 7){
                 var g;
                 for (g = 0; g < 2; g++) {
                     var coldiv = document.createElement("div");
-                    coldiv.style.width = "45vw";
+                    coldiv.style.width = "38vw";
                     coldiv.style.height = "100px";
                     coldiv.style.backgroundColor = "blue";
                     coldiv.style.display = "flex";
-                    coldiv.style.flexdirection = "column";
+                    coldiv.style.flexDirection = "column";
                     coldiv.setAttribute("id", "coldiv" + g);
 
                     document.getElementById("contentcont").appendChild(coldiv);
                 }
-            }
+            } //two columns
         }
 
         }
